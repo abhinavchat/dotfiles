60884d5650e65fb808af3cceb350a78925b91270
Abhinav Chaturvedi <abhinav.c@live.in>
2024-09-29 16:05:02 +0530

    Modify the config files

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -1,10 +1,13 @@
-return { 
-    {
-      'nvim-telescope/telescope.nvim', 
-      tag = '0.1.6',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+return {
+  {
+    'catppuccin/nvim',
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme "catppuccin"
+    end
   },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
@@ -13,5 +16,29 @@ return {
       "nvim-tree/nvim-web-devicons",
       "MunifTanjim/nui.nvim",
     },
+    config = function()
+        vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    end
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+        local builtin = require("telescope.builtin")
+        vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            ensure_installed = {"lua", "python", "javascript", "rust"},
+            highlight = { enable = true },
+            indent = { enable = true }
+        })
+    end
   }
 }
